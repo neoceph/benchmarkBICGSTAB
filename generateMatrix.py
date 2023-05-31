d322452cfc481b400ce0b723baee3e8585bc1e75
Abdullah Amin <nirjhor18@gmail.com>
2023-05-30 22:41:26 -0500

    Sparse Matrix

--- generateMatrix.py
+++ generateMatrix.py
@@ -9,7 +9,16 @@ if not os.path.exists(directoryName):
 dimensions = [10, 100, 1000, 2000, 3000, 4000, 5000]
 
 for dimension in dimensions:
-    matrixA = np.random.rand(dimension, dimension)
+    # generating tridiagonal matrix
+    matrixA = np.zeros((dimension, dimension))
+    for i in range(dimension):
+        for j in range(dimension):
+            if i == j:
+                matrixA[i][j] = np.random.rand()
+            elif i == j + 1 or i == j - 1:
+                matrixA[i][j] = np.random.rand()
+            else:
+                matrixA[i][j] = 0
     matrixb = np.random.rand(dimension, 1)
 
     matrixAFilename = directoryName + "/matrixA" + str(dimension) + ".csv"
